e6d7a85efbab37bbad096ad975f08cf42fd9284c
adria <adriana.krush@gmail.com>
2022-12-18 21:02:28 +0200

    STG

--- src/newckass.java
+++ src/newckass.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class newckass {
+    public static void main(String[] args) {
+        Scanner num=new Scanner(System.in);
+        System.out.println("enter number");
+        int number = num.nextInt();
+
+        if(number<=100){
+            System.out.println("great");
+            number++;
+            System.out.println(number);
+        }else {
+            System.out.println("Well");
+            number--;
+            System.out.println(number);
+        }
+    }
 }
